0698fb278fa0919bd22ea0e7da186e82d3274539
kasper <kasperkbf@hotmail.com>
2022-05-22 21:58:11 +0200

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express  from "express";
+import cors from "cors";
+import tasks from "./api/tasks.route.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/tasks", tasks);
+app.use("*", (req, res) => res.status(404).json({error: "not found"}));
+
+export default app;
