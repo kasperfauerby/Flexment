62ff7c615d09fb2f5dd928f08d0739132c423058
kasper <kasperkbf@hotmail.com>
2022-05-23 15:04:23 +0200

    'review' crud

--- backend/api/tasks.route.js
+++ backend/api/tasks.route.js
@@ -7,4 +7,10 @@ router.route("/").get(TasksCtrl.apiGetTasks);
 router.route("/id/:id").get(TasksCtrl.apiGetTaskById);
 router.route("/cuisines").get(TasksCtrl.apiGetTaskCuisines);
 
+router
+  .route("/review")
+  .post(function(req, res) { TasksCtrl.apiPostReview })
+  .put(function(req, res) { TasksCtrl.apiUpdateReview })
+  .delete(function(req, res) { TasksCtrl.apiDeleteReview })
+
 export default router
