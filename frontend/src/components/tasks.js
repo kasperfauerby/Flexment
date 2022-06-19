402f20b785966c5f6329b0f1bc2ef2f154b9ee3b
kasper <kasperkbf@hotmail.com>
2022-06-19 14:24:58 +0200

    description lorem ipsum

--- frontend/src/components/tasks.js
+++ frontend/src/components/tasks.js
@@ -34,10 +34,14 @@ const Task = props => {
           <h5>{task.name}</h5>
           <p>
             <strong>Company: </strong>{task.company}<br/>
-            <strong>Programming language: </strong>{task.p_language}
+            <strong>Programming language: </strong>{task.p_language}<br/>
+            <strong>Description: </strong> <br/>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vivamus arcu felis bibendum ut. Volutpat commodo sed egestas egestas fringilla phasellus faucibus. Enim facilisis gravida neque convallis a cras semper. Dictumst quisque sagittis purus sit amet volutpat consequat mauris nunc. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Fusce id velit ut tortor pretium viverra. Eu tincidunt tortor aliquam nulla facilisi cras. Malesuada pellentesque elit eget gravida cum sociis. Vitae proin sagittis nisl rhoncus mattis rhoncus urna. Iaculis eu non diam phasellus vestibulum lorem sed risus ultricies. Amet purus gravida quis blandit turpis cursus. Fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque.
+
+Eu non diam phasellus vestibulum lorem sed risus ultricies tristique. Aliquet porttitor lacus luctus accumsan tortor posuere ac. Enim sit amet venenatis urna. At volutpat diam ut venenatis. Etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Pulvinar mattis nunc sed blandit libero volutpat sed cras ornare. Et malesuada fames ac turpis egestas maecenas pharetra. Pharetra massa massa ultricies mi quis hendrerit. Aliquam faucibus purus in massa tempor nec feugiat nisl. Morbi enim nunc faucibus a pellentesque sit amet. Cursus euismod quis viverra nibh. Facilisis leo vel fringilla est ullamcorper. Tortor dignissim convallis aenean et.
           </p>
           <Link to={"/tasks/" + props.match.params.id + "/review"} className="btn btn-primary">
-            Add Review
+            Contact
           </Link>
         </div>
       ) : (
